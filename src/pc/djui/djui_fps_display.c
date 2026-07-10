2703edc096afe565b24199f9d74bbc1e4bd66855
Cooliokid956 <68075390+Cooliokid956@users.noreply.github.com>
2026-07-10 13:57:36 -0500

    Shorten
    Please @PeachyPeachSM64

--- src/pc/djui/djui_fps_display.c
+++ src/pc/djui/djui_fps_display.c
@@ -12,7 +12,7 @@ void djui_fps_display_update(u32 fps) {
     if (configShowFPS && sFpsDisplay != NULL) {
         char fpsText[12] = "";
         fps = fps > 99999 ? 99999 : fps; // Prevent overflowing the FPS display (cap at 99999)
-        snprintf(fpsText, 12, "FPS: \\#ffffff\\%d", fps);
+        snprintf(fpsText, 12, "FPS: \\#fff\\%d", fps);
         djui_text_set_text(sFpsDisplay->text, fpsText);
     }
 }
